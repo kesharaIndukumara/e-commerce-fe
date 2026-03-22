00462d68d9b3c5e8f065615610f5e3b922157dd2
kesharaIndukumara <kavindukeahara2016@gmail.com>
2026-03-23 01:23:40 +0530

    add product listing and categories components; implement product data structure

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { ProductType } from "@/types";
+
+const ProductCard = ({product} : {product:ProductType}) =>{
+    return (
+        <div className='shadow-lg rounded-lg overflow-hidden '>ProductCard</div>
+    )
+}
+
+export default ProductCard;
