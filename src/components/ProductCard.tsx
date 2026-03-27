c82626f1ba48268a9041c5a096f1f4ebb6d55208
kesharaIndukumara <kavindukeahara2016@gmail.com>
2026-03-27 18:35:54 +0530

    add product card component; implement image linking and styling

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,10 +1,19 @@
 "use client"
 
 import { ProductType } from "@/types";
+import Link from "next/link";
+import Image from "next/image";
 
 const ProductCard = ({product} : {product:ProductType}) =>{
+
     return (
-        <div className='shadow-lg rounded-lg overflow-hidden '>ProductCard</div>
+        <div  className='shadow-lg rounded-lg overflow-hidden '>
+            <Link href={`/products/${product.id}`}>
+                <div className="relative aspect-[2/3]">
+                    <Image src={product.images[product.colors[0]]} alt={product.name} fill className="object-cover hover:scal-105 transition-all duration-300" />
+                </div>
+            </Link>            
+        </div>
     )
 }
 
